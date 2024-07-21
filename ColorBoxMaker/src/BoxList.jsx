7ccb432d6c4d6b0596d52c295c0fef10989e5e3b
JJ <jonjones320@gmail.com>
2024-07-20 18:25:29 -0700

    Added BoxList component which rendersthe state and Box component and NewBoxForm component.

---
+++ ColorBoxMaker/src/BoxList.jsx
@@ -0,0 +1,30 @@
+import React, { useState } from "react";
+import { v4 as uuid } from 'uuid';
+import Box from "./Box"
+import NewBoxForm from "./NewBoxForm";
+
+
+const BoxList = () => {
+  const INITIAL_STATE = [
+    { id: uuid(), color: 'Orange', width: 50, height: 50 },
+    { id: uuid(), color: 'Green', width: 50, height: 50 }
+  ]
+  const [boxes, setBoxes] = useState(INITIAL_STATE);
+  const addBox = (newBox) => {
+    setItems(boxes => [...boxes, { ...newBox, id: uuid() }])
+  }
+  return (
+    <div>
+      <h3>Box List</h3>
+      <NewBoxForm addBox={addBox} />
+      <div>
+        {boxes.map(({ id, color, width, height }) => <Box id={id} color={color} width={width} height={height} key={id} />)}
+      </div>
+    </div>
+  )
+
+}
+
+export default BoxList;
+
+
